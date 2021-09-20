5c7cf8564bacbe5a72100438fa8a11b9d191d6d1
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-20 19:34:59 -0400

    Add Form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Form from "./Components/Form";
+
+import { GlobalStyle } from "./Styles";
+// import styled from "styled-components/macro";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Header />
+      <Form />
       <Footer />
     </div>
   );
