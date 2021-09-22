c20b904025dea0253e4ed8262793f2e5da2c3efe
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-22 11:04:15 -0400

    WIP: Adding styles to Header Component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { IoLogoFirebase, IoLogoReact } from "react-icons/io5";
+import { SiStyledComponents, SiGooglekeep } from "react-icons/si";
 
 function Header() {
   return (
     <header>
+      <IoLogoFirebase size={50} />
+      <IoLogoReact size={50} />
+      <SiStyledComponents size={50} />
+      <SiGooglekeep size={50} />
+
       <h1>Google Keep</h1>
     </header>
   );
