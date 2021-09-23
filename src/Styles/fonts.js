f40ee67b1054e54125bfd0d3904b52093fca8a13
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-23 19:59:47 -0400

    Adjust styles as needed

--- src/Styles/fonts.js
+++ src/Styles/fonts.js
@@ -3,28 +3,28 @@ import { css } from "styled-components/macro";
 const fonts = css`
   @font-face {
     font-family: "Fira Code";
-    src: url("../../public/fonts/FiraCode-Light.ttf") format("truetype");
+    src: url("../assets/fonts/FiraCode-Light.ttf") format("truetype");
     font-weight: 300;
     font-style: normal;
   }
 
   @font-face {
     font-family: "Fira Code";
-    src: url("../../public/fonts/FiraCode-Regular.ttf") format("truetype");
+    src: url("../assets/fonts/FiraCode-Regular.ttf") format("truetype");
     font-weight: 400;
     font-style: normal;
   }
 
   @font-face {
     font-family: "Fira Code";
-    src: url("../../public/fonts/FiraCode-Medium.ttf") format("truetype");
+    src: url("../assets/fonts/FiraCode-Medium.ttf") format("truetype");
     font-weight: 500;
     font-style: normal;
   }
 
   @font-face {
     font-family: "Fira Code";
-    src: url("../../public/fonts/FiraCode-Bold.ttf") format("truetype");
+    src: url("../assets/fonts/FiraCode-Bold.ttf") format("truetype");
     font-weight: 700;
     font-style: normal;
   }
