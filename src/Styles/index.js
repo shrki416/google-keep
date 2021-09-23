a5b5980bb0c53d9b1c73dd4125b34c4c88c03fa2
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-23 19:59:57 -0400

    Adjust styles as needed

--- src/Styles/index.js
+++ src/Styles/index.js
@@ -1,2 +1,4 @@
 export { default as GlobalStyle } from "./GlobalStyle";
 export { default as StyledHeader } from "./StyledHeader";
+export { default as StyledForm } from "./StyledForm";
+export { default as StyledNote } from "./StyledNote";
