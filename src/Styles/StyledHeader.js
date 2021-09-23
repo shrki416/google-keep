19fabcf2e0374bafd6bdb8aa556ba59586283ffa
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-23 17:39:56 -0400

    Style Header

--- src/Styles/StyledHeader.js
+++ src/Styles/StyledHeader.js
@@ -2,6 +2,11 @@ import styled from "styled-components/macro";
 
 const StyledHeader = styled.header`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--yellow);
+  padding: 16px 32px;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.3);
 `;
 
 export default StyledHeader;
