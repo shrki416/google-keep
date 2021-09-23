179f922568cd4553b9af4f7c39a965ac8f6a6057
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-23 19:58:49 -0400

    Refactor

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,10 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+import { StyledForm } from "../Styles";
+import AddIcon from "@material-ui/icons/Add";
+import { Fab, Zoom } from "@material-ui/core";
+import Note from "./Note";
 
 export default function Form() {
+  const [notes, setNotes] = useState([]);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const { title, content } = note;
+
+  const change = (e) => {
+    const { name, value } = e.target;
+    setNote({ ...note, [name]: value });
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    const newNote = { ...note, id: Date.now() };
+    setNotes([...notes, newNote]);
+    clearNote();
+  };
+
+  const clearNote = () => {
+    setNote({
+      title: "",
+      content: "",
+    });
+  };
+
+  const handleDelete = (id) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="title" name="tile" value="" />
-      <textarea name="content" value="" rows="10"></textarea>
-    </form>
+    <>
+      <StyledForm>
+        {isExpanded && (
+          <input
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => change(e)}
+            placeholder="title"
+          />
+        )}
+
+        <textarea
+          name="content"
+          rows={isExpanded ? 5 : 1}
+          value={content}
+          onChange={(e) => change(e)}
+          onClick={() => setIsExpanded(true)}
+          placeholder="Keep a note..."
+        />
+
+        <Zoom in={isExpanded}>
+          <Fab>
+            <AddIcon onClick={submit} />
+          </Fab>
+        </Zoom>
+      </StyledForm>
+      {notes.map((note, index) => (
+        <Note key={index} note={note} handleDelete={handleDelete} />
+      ))}
+    </>
   );
 }
