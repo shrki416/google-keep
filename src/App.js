7e1ea06f28b32ee96c8481c86a4fda3fdefa1a08
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-26 17:08:37 -0400

    Refactor

--- src/App.js
+++ src/App.js
@@ -4,27 +4,6 @@ import Logos from "./Components/Logos";
 
 import { GlobalStyle } from "./Styles";
 
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDBeKxr6xyAYWw8m-fBxtoZ5R2q2tm66WY",
-  authDomain: "notes-1a38b.firebaseapp.com",
-  databaseURL: "https://notes-1a38b-default-rtdb.firebaseio.com",
-  projectId: "notes-1a38b",
-  storageBucket: "notes-1a38b.appspot.com",
-  messagingSenderId: "662553462650",
-  appId: "1:662553462650:web:af60643bce38c4c2fb38c3",
-  measurementId: "G-1QC32VHN7Y",
-};
-
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-const database = getDatabase();
-console.log(database);
-
 function App() {
   return (
     <div className="App">
