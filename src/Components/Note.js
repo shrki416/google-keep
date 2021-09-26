ae668da3eea4244cf7e1347bece1446b975dec6d
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2021-09-26 17:10:46 -0400

    Notes populating correctly and persisting

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -1,9 +1,11 @@
-import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
 import { StyledNote } from "../Styles";
 
-function Note({ note, handleDelete }) {
+function Note({ note, handleDelete, handleEdit }) {
   const { title, content } = note;
+
+  console.log(note);
 
   return (
     <StyledNote>
@@ -11,6 +13,9 @@ function Note({ note, handleDelete }) {
       <p>{content}</p>
       <button onClick={() => handleDelete(note.id)}>
         <DeleteIcon />
+      </button>
+      <button onClick={() => handleEdit(note.id)}>
+        <EditIcon />
       </button>
     </StyledNote>
   );
